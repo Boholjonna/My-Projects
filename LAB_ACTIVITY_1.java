1471dbe4890bc7365a2d39865d3ca7c226f73577
Jonna Bohol BSCPE 1-G DAY PROGRAM <159886481+Boholjonna@users.noreply.github.com>
2024-02-13 17:28:50 +0800

    Add files via upload

---
+++ LAB_ACTIVITY_1.java
@@ -0,0 +1,13 @@
+
+public class LAB_ACTIVITY_1 {
+
+    public static void main(String[] args) {
+        System.out.println("NAME: Bohol, Jonna");
+         System.out.println("AGE: 19");
+          System.out.println("GENDER: Female");
+           System.out.println("ADDRESS: Hagnaya, San Remigio, Cebu");
+            System.out.println("CIVIL STATUS: Single");
+             System.out.println("# OF BROTHERS/SISTERS: 4");
+        
+    }
+}
